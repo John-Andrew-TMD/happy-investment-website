ddef2744889c12e1712cdf50bbe20fa16dc20e64
John-Andrew-TMD <372900407@qq.com>
2024-03-15 16:52:18 +0800

    fix:吸顶组件相关功能

--- layouts/components/rightNav/index.vue
+++ layouts/components/rightNav/index.vue
@@ -3,16 +3,54 @@
     <div class="fixed-quick-menu">
       <ul class="fixed-quick-tab">
         <li>
-          <el-icon><Link /></el-icon>
+          <el-icon><Link /> </el-icon>
+          <div class="tab-content">
+            <div class="fixed-quick-header">
+              <h3>招商网络招商端</h3>
+              <p>扫码认领单位 快速直连企业</p>
+            </div>
+            <img
+              src="https://static.zhaoshang.net/front/images/fixed_invite_qrcode@2x.jpg?2023042501"
+              alt="招商网络招商端"
+            />
+            <div class="fixed-quick-text">
+              真实项目每日上新<br />一通电话沟通合作
+            </div>
+          </div>
         </li>
         <li>
           <el-icon><Memo /></el-icon>
+          <div class="tab-content">
+            <div class="fixed-quick-header">
+              <h3>招商要参公众号</h3>
+              <p>抓住招商引资新方向</p>
+            </div>
+            <img
+              src="https://static.zhaoshang.net/front/images/fixed_invite_offical@2x.jpg?2023042501"
+              alt="招商要参公众号"
+            />
+            <div class="fixed-quick-text">前沿招商资讯<br />项目对接交流</div>
+          </div>
         </li>
         <li>
           <el-icon><User /></el-icon>
+          <div class="tab-content">
+            <div class="fixed-quick-header">
+              <h3>招商圈子</h3>
+              <p>招商人的在线交流互动社区</p>
+            </div>
+            <img
+              src="https://static.zhaoshang.net/front/images/fixed_invite_circle@2x.jpg?2023042501"
+              alt="招商圈子"
+            />
+            <div class="fixed-quick-text">
+              扫码申请加入<br /><span>“招商项目对接交流群”</span>
+            </div>
+          </div>
         </li>
         <li class="feedback">
           <el-icon><Box /></el-icon>
+          <div class="tab-content"></div>
         </li>
       </ul>
       <ul class="fixed-quick-content">
